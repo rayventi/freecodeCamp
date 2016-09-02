70b4d6731ed0e575a555737420baed2571566c19
wenhui-lei <ruanwuqingyan@hotmail.com>
2016-09-03 00:19:11 +0800

    点击后变化图片
    js 数组的应用

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-	var img1on="img/22on.gif";
+	var img_on=new Array();
+	img_on[0]="img/22on.gif";
+	img_on[1]="img/33on.gif";
+	img_on[2]="img/tvon.gif";
+	var img_off=new Array();
+	img_off[0]="img/22.gif";
+	img_off[1]="img/33.gif";
+	img_off[2]="img/tv.png";
 	$("#mascot .img_btns a").each(function(i){
 		$(this).hover(function(){
-			$("this").children("img").attr("src","img/22on.gif");
-			$(".div_mascots .mascots"+(i+1)).show();
-		}),
-		function(){
-			$("this").children("img").attr("src","img/22on.gif");
-			$(".div_mascots .mascots"+(i+1)).hide();
+			$(this).children("img").attr("src",img_on[i]);
+			$(".div_mascots .mascots"+(i+1)).fadeIn("slow");
+		},function(){
+			$(this).children("img").attr("src",img_off[i]);
+			$(".div_mascots .mascots"+(i+1)).fadeOut("fast");
 		}
+		)
 	})
 })
